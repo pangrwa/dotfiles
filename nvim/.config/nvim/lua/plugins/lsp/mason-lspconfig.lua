ccf0a8723fb89810d8e5c762bd7d09e88df7fcff
Pang Rui Wei <>
2025-07-28 22:41:17 +0800

    disable automatic enable servers to prevent dup lsp clients

--- nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
     ensure_installed = { "lua_ls", "clangd", "neocmake" },
-    automatic_enable = true,
+    automatic_enable = false,
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
